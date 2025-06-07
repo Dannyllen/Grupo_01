79582cd578f39ca5c6eb159dc1a9933942634868
Daniela Astudillo León <danvastu@espol.edu.ec>
2025-06-07 11:17:22 -0500

    Creacion del proyecto con la clase Main predeterminada

---
+++ src/main/java/com/mycompany/grupo_01/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.grupo_01;
+
+/**
+ *
+ * @author dval0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
